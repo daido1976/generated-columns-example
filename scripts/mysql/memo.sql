6b4264c476be908186f25707b7d4fed2bc641862
daido1976 <daido1976@gmail.com>
2023-09-10 17:41:00 +0900

    If the limit is 1000, it is not that slow, so increase the limit

--- scripts/mysql/memo.sql
+++ scripts/mysql/memo.sql
@@ -3,26 +3,26 @@ ALTER TABLE users ADD COLUMN email_domain VARCHAR(255) GENERATED ALWAYS AS (SUBS
 ALTER TABLE users ADD INDEX idx_email_domain (email_domain);
 
 -- Before
-SELECT email FROM users WHERE email LIKE "%example.com" LIMIT 1000;
+SELECT email FROM users WHERE email LIKE "%example.com" LIMIT 2000;
 
 -- After
-SELECT email FROM users WHERE email_domain = "example.com" LIMIT 1000;
+SELECT email FROM users WHERE email_domain = "example.com" LIMIT 2000;
 
 -- 2. optimization for email domain search with fulltext index
 ALTER TABLE users ADD FULLTEXT INDEX idx_fulltext_email (email);
 
 -- Before
-SELECT email FROM users WHERE email LIKE "%example.com" LIMIT 1000;
+SELECT email FROM users WHERE email LIKE "%example.com" LIMIT 2000;
 
 -- After
-SELECT email FROM users WHERE MATCH(email) AGAINST('*example.com' IN BOOLEAN MODE) LIMIT 1000;
+SELECT email FROM users WHERE MATCH(email) AGAINST('*example.com' IN BOOLEAN MODE) LIMIT 2000;
 
 -- 3. optimization for email domain search with generated column for sorting
 ALTER TABLE users ADD COLUMN email_domain VARCHAR(255) GENERATED ALWAYS AS (SUBSTRING_INDEX(email, '@', -1)) VIRTUAL;
 ALTER TABLE users ADD INDEX idx_email_domain_last_login_at (email_domain, last_login_at);
 
 -- Before
-SELECT email FROM users WHERE email LIKE "%example.com" ORDER BY last_login_at LIMIT 1000;
+SELECT email FROM users WHERE email LIKE "%example.com" ORDER BY last_login_at LIMIT 2000;
 
 -- After
-SELECT email FROM users WHERE email_domain = "example.com" ORDER BY last_login_at LIMIT 1000;
+SELECT email FROM users WHERE email_domain = "example.com" ORDER BY last_login_at LIMIT 2000;
