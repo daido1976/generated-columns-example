323c21fd914c11fe101c3ae82e2e01f80b7f9ab7
daido1976 <daido1976@gmail.com>
2023-09-10 16:38:35 +0900

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+```sh
+$ docker compose up -d
+$ ./scripts/mysql/setup.sh 10000000
+```
--- scripts/mysql/setup.sh
+++ scripts/mysql/setup.sh
@@ -3,11 +3,11 @@
 # create-table.sql を実行して users テーブルを作成
 docker compose exec mysql mysql -u root --password=root -D generated_columns_example -e "$(cat "$(dirname "$0")/create-table.sql")"
 
-# コマンドライン引数で指定したレコード数を取得
-RECORD_COUNT=$1
-
 # 一度に処理するレコードの数（多すぎると argument list too long が出る）
 BATCH_SIZE=2000
+
+# コマンドライン引数で指定したレコード数を取得
+RECORD_COUNT=${1:-$BATCH_SIZE}
 
 # スクリプトの開始時間を取得
 START_TIME=$(date +%s)
